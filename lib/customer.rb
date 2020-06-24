fd854edc39498bc1a1edc5b6b32d93ec284aa5dd
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-24 03:37:14 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,37 @@
+require "pry"
 class Customer
   
+    attr_accessor :name, :age
+
+    @all = []
+
+    def initialize(name, age)
+
+        @name = name
+        @age = age
+        @@all << self
+
+    end
+
+    def self.all
+
+        @@all
+
+    end
+
+    def new_meal(waiter, total, tip)
+
+        Meal.new(waiter, self, total, tip)
+
+    end
+
+    def meals
+
+
+    end
+
+    def waiters
+
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,35 @@
 class Waiter
 
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+
+        @name = name
+        @age = age
+        @@all << self
+
+    end
+
+    def self.all
+        
+        @@all
+
+    end
+
+    def new_meal
+
+
+    end
+
+    def meals
+
+    end
+
+    def best_tipper
+
+
+    end
+
 end
